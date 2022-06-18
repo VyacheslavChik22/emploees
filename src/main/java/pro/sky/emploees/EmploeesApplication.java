628412a266b1cd3941c6256360969ca9ebc28f98
VyacheslavCHik <asyofka@mail.ru>
2022-06-19 02:23:23 +0300

    first commit

---
+++ src/main/java/pro/sky/emploees/EmploeesApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.emploees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploeesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmploeesApplication.class, args);
+    }
+
+}
